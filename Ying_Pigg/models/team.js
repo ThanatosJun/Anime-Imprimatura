f9dfc0071855cff60932e14287960f68e791c969
pizzaInmystomach <134912847+pizzaInmystomach@users.noreply.github.com>
2024-07-04 03:46:00 +0800

    y

---
+++ Ying_Pigg/models/team.js
@@ -0,0 +1,23 @@
+const { MaxKey } = require('mongodb');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//create schema
+const teamSchema = new mongoose.Schema({
+    team_id: {
+        type: Number,
+        required: true,
+    }, 
+    team_name: {
+        type: String,
+        required: true,
+        min: 0,
+        max: 45
+    }
+});
+
+//define model
+const teamModel = mongoose.model('Team', teamSchema);
+
+//export model
+module.exports = teamModel
