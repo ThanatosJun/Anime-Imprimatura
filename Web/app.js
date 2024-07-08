b52412efcf3ead85fbdb393d6faa9a8ad31a04bd
ying0607 <chingying0607@gmail.com>
2024-07-08 14:45:12 +0800

    web update

--- Web/app.js
+++ Web/app.js
@@ -3,17 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var accountManagementRouter = require('./routes/account_management');
-var createAccountRouter = require('./routes/create_account');
-var galleryRouter = require('./routes/gallery');
-var generateVisitorRouter = require('./routes/generate_visitor');
-var generatedVisitorRouter = require('./routes/generated_visitor');
-var loginRouter = require('./routes/login');
-var teamGalleryFRouter = require('./routes/team_gallery_f');
-var teamGalleryTRouter = require('./routes/team_gallery_t');
+const connectMongoDB = require('./database'); // 引入數據庫連接模塊
+const routes = require('./routes'); // 引入路由模塊
 
 var db = require('./database');
 
@@ -23,22 +14,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use('/', routes);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/account_management', accountManagementRouter);
-app.use('/create_account', createAccountRouter);
-app.use('/gallery', galleryRouter);
-app.use('/generate_visitor', generateVisitorRouter);
-app.use('/generated_visitor', generatedVisitorRouter);
-app.use('/login', loginRouter);
-app.use('/team_gallery_f', teamGalleryFRouter);
-app.use('/team_gallery_t', teamGalleryTRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Web/database.js
+++ Web/database.js
@@ -1,13 +1,31 @@
-//Import the mongoose module
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
 
-//Set up default mongoose connection
-var mongoDB = "mongodb://127.0.0.1/my_database";
-mongoose.connect(mongoDB);
-// Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
-//Get the default connection
+// 加載環境變量
+dotenv.config();
+
+// 使用環境變量中的 MONGO_URI，並進行錯誤檢查
+const uri = process.env.MONGO_URI;
+if (!uri) {
+  throw new Error('MONGO_URI is not defined in the environment variables');
+}
+
+// 打印連接 URI（隱藏密碼）
+console.log('Connecting with URI:', uri.replace(/:[^:]*@/, ':****@'));
+
+// connect to mongoose
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB', err));
+
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+module.exports = {
+  db,
+  mongoose,
+  bcrypt
+};
