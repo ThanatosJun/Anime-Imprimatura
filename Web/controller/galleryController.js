b01dc904f89d03035aaf2cf81e29ff52ac9c7543
pizzaInmystomach <134912847+pizzaInmystomach@users.noreply.github.com>
2024-07-10 21:05:32 +0800

    update gallery

--- Web/controller/galleryController.js
+++ Web/controller/galleryController.js
@@ -3,13 +3,13 @@
 require("dotenv").config();
 const image = require('../models/image');
 
-exports.getGallery = async(req, res) =>{
-    try{
-        // acquire images by user_id
-        const images = await Image.find({ userID: req.user._id });
-        res.render('gallery', { image });
-    }catch(err){
-        console.error('Error fetching gallery: ', err);
-        res.status(500).send('Error fetching gallery. ');
+exports.getGallery = async (req, res) => {
+    try {
+      const images = await Image.find({ userId: req.user._id }); // 根據用戶 ID 獲取圖片
+      console.log('Fetched images:', images); // 添加日誌來檢查數據
+      res.render('gallery', { images });
+    } catch (err) {
+      console.error('Error fetching gallery:', err);
+      res.status(500).send('Error fetching gallery');
     }
-}
+  };
