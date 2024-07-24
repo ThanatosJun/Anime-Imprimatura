62f6f5c14e5c7dfe5ab22f431c252dab427ff79a
ying0607 <chingying0607@gmail.com>
2024-07-24 20:05:21 +0800

    add comment

--- Web/routes/generate_detect_visitor.js
+++ Web/routes/generate_detect_visitor.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('generate_detect_visitor', { title: 'Generate Detect Visitor' });
 });
 
+router.get('/train', function(req, res, next) {
+  res.render('generate_train_visitor', { title: 'Generate train Visitor' });
+});
+
 module.exports = router;
