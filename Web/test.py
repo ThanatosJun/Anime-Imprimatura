0189ee853c2b3c7511e988e23fc686a010d6f206
pizzaInmystomach <hsiaochu17@gmail.com>
2024-10-05 15:57:06 +0800

    add flex and fast

--- Web/test.py
+++ Web/test.py
@@ -86,16 +86,15 @@ def flex_detect_image():
     data = request.get_json()  # 获取POST请求的JSON数据
     user_id = data.get('user_id')
     character_name = data.get('character_name')
-    model = data.get('model')
     chd_path = data.get('chd_path')
     chs_path = data.get('chs_path')
 
     print('Now executing "Detect". ')
-    import CHD_detect_v2
+    import CHS_detect_flow2
     CHS_save_dir = None
     try:
         print(f'Received detect request with character_name: {character_name}, model: {model}, chd_path: {chd_path}, chs_path: {chs_path}')
-        CHS_save_dir = CHD_detect_v2.main( user_id, character_name, chd_path, chs_path )
+        CHS_save_dir = CHS_detect_flow2.main( user_id, character_name, chd_path, chs_path )
         print(CHS_save_dir)            
         
         output = "Detect script executed successfully."
@@ -104,6 +103,7 @@ def flex_detect_image():
         print(f'Error during detecting: {e}')
         return jsonify({'status': 'error', 'error': str(e) })
 
+# fast
 @app.route('/fast', methods=['POST'])
 def fast_segment_image():
     data = request.get_json()  # 获取POST请求的JSON数据
