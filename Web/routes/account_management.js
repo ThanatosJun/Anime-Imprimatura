1ac46010f4d0990ea882d0a55f29bd0b16ca0bb2
ThanatosJun <thanatosjun.com.tw>
2025-01-01 12:12:25 +0800

    clean

--- Web/routes/account_management.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET account management page. */
-router.get('/', function(req, res, next) {
-  res.render('account_management', { title: 'Account Management' });
-});
-
-module.exports = router;
